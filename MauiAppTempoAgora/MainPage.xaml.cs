5ec12a702169a683c7ff92d75dd849a4ef349c0c
jvzin8040 <joaovitorvcn07@gmail.com>
2025-04-05 11:58:06 -0300

    V3

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -12,7 +12,6 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-
         private async void Button_Clicked(object sender, EventArgs e)
         {
             try
@@ -21,30 +20,41 @@ namespace MauiAppTempoAgora
                 {
                     Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-                    if (t != null) 
+                    if (t != null)
                     {
                         string dados_previsao = $"Latitude: {t.lat}\n" +
                             $"Longitude: {t.lon}\n" +
                             $"Nascer do Sol: {t.sunrise}\n" +
                             $"Pôr do Sol: {t.sunset}\n" +
                             $"Temp Máx: {t.temp_max}°C\n" +
-                            $"Temp Mín: {t.temp_min}°C\n";
+                            $"Temp Mín: {t.temp_min}°C\n" +
+                            $"Velocidade do Vento: {t.speed} m/s\n" +
+                            $"Visibilidade: {t.visibility} m\n" +
+                            $"Descrição: {t.description}\n";
 
                         lbl_res.Text = dados_previsao;
                     }
                     else
                     {
-                        lbl_res.Text = "Cidade não encontrada/Sem Dados de Previsão";
+                        lbl_res.Text = "Cidade não encontrada";
                     }
                 }
                 else
                 {
                     lbl_res.Text = "Digite o nome de uma cidade";
                 }
-            }
+
+            } // fecha try
             catch (Exception ex)
             {
-                await DisplayAlert("Ops", ex.Message, "OK");
+                if (ex.Message == "No such host is known. (api.openweathermap.org:443)")
+                {
+                    await DisplayAlert("Ops", "Sem conexão com a internet", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Ops", ex.Message, "OK");
+                }
             }
         }
     }
